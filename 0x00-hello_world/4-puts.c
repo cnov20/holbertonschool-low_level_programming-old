f82e5dccbc501ba4a1b4c395cd95251bf8f8d6d2
cnov20 <128@holbertonschool.com>
2017-02-09 02:24:57 +0000

    Task 3 - Comment fix - full function and return value descriptions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 /**
- * puts_function - Puts specified text to standard output
+ * main - Entry point of program for execution
+ *
+ * puts - Puts specified text to standard output
+ *
+ * Return: (0)
  */
-int main()
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
