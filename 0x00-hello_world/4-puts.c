2b83fb08aff87383252dda0ad6f0bdf8dfae7eb9
cnov20 <128@holbertonschool.com>
2017-02-09 02:15:57 +0000

    Task 3 - Simple Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void)
+/**
+ * puts_function - Puts specified text to standard output
+ */
+int main()
 {
-        puts("Programming is like building a multilingual puzzle");
-        return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
