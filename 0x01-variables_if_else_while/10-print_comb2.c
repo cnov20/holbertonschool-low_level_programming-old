39bdf56fc0f21ffbb2bb2c5cda074b005da5b79c
cnov20 <128@holbertonschool.com>
2017-02-10 07:22:18 +0000

    Task 10 Correct

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,16 +9,23 @@
 
 int main(void)
 {
-	int i = 0;
+  int i;
+  int j; 
 
-	while (i < 100)
+  for (i = 0; i < 10; i++)
+    {
+      for (j = 0; j < 10; j++)
 	{
-		putchar(i);
-		if (i != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}i++;
+	  putchar(i + '0');
+	  putchar(j + '0');
+	  
+	  if (i != 9 || j != 9)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
 	}
-		return (0);
+    }
+  putchar('\n');
+  return (0);
 }
