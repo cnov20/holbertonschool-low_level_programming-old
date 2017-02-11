e6d3e574c488a8ca73f32d61c7c2ca332ba40bc6
cnov20 <128@holbertonschool.com>
2017-02-11 08:54:24 +0000

    Task 13 Final Fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,6 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
@@ -11,14 +12,14 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 1; j < 100; j++)
+		for (j =  i + 1; j < 100; j++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
